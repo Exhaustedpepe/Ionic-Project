2bfc9ebd57c03ff8c3a3fd918c8844be6194dd5c
JungGarY <garfeijung@gmail.com>
2022-04-20 15:02:33 -0400

    add page functioning

--- MobileAppProject/src/app/add-page/add-page.page.ts
+++ MobileAppProject/src/app/add-page/add-page.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DbService } from '../services/db.service';
 
 @Component({
   selector: 'app-add-page',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-page.page.scss'],
 })
 export class AddPagePage implements OnInit {
+  addForm: FormGroup;
 
-  constructor() { }
+  constructor(private dbService: DbService, public formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      restaurant_name: [''],
+      address: [''],
+      rating: [''],
+      tags: [''],
+      description: [''],
+    })
+    await this.dbService.init()
+  }
+
+  onSubmit(){
+    this.dbService.addRestaurant(this.addForm.value)
   }
 
 }
