a80bca2dc6ab655c038f3a5db78e3c240510def5
JungGarY <garfeijung@gmail.com>
2022-04-20 18:33:55 -0400

    edit page done

--- MobileAppProject/src/app/edit-page/edit-page.page.ts
+++ MobileAppProject/src/app/edit-page/edit-page.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService } from '../services/db.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Restaurant } from '../services/restaurants';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-page',
@@ -7,11 +10,35 @@ import { DbService } from '../services/db.service';
   styleUrls: ['./edit-page.page.scss'],
 })
 export class EditPagePage implements OnInit {
+  editForm: FormGroup;
+  restaurant: Restaurant;
 
-  constructor(private dbService: DbService) { }
+  constructor(private dbService: DbService, 
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private actRoute: ActivatedRoute) { 
+    dbService.getRestaurantById(actRoute.snapshot.params.id).then(restaurant => {
+      this.restaurant = this.restaurant
+      this.editForm = this.formBuilder.group({
+        id: this.restaurant.id,
+        restaurant_name: [this.restaurant.restaurant_name],
+        address: [this.restaurant.address],
+        rating: [this.restaurant.rating],
+        tags: [this.restaurant.tags],
+        description: [this.restaurant.description],
+      })
+    })
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.dbService.init()
   }
   
+  onSubmit() {
+    this.dbService.updateRestaurant(this.editForm.value)
+  }
+  deleteRestaurant(id){
+    this.dbService.deleteRestaurantById(id)
+  }
 
 }
